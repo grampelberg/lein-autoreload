74e65e6728b7ad67d0ff0690572b60aae9be450e
John Skilbeck <john.skilbeck@gmail.com>
2016-09-28 01:28:07 -0700

    works how I want it

--- src/lein_autoreload/plugin.clj
+++ src/lein_autoreload/plugin.clj
@@ -11,7 +11,7 @@
       (eval/eval-in (assoc project :eval-in :nrepl)
         '(do
           (require 'clojure.tools.namespace.repl)
-          (clojure.tools.namespace.repl/refresh))))
+          (clojure.tools.namespace.repl/refresh :after 'clojure.main/repl-prompt))))
     (catch Throwable e (println (.getStackTrace e))))
   (Thread/sleep 1000))
 
