07d33a057cbe92619332fa5072a0a67cd134765c
komcrad <konradt95@gmail.com>
2019-04-10 15:49:55 -0500

    updates deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject komcrad/lein-autoreload "0.1.2"
+(defproject komcrad/lein-autoreload "0.1.4-SNAPSHOT"
   :description "When running the repl, reload every time a file is saved."
   :url "https://github.com/komcrad/lein-autoreload"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [
     [leiningen-core "2.7.0"]
-    [ns-tracker "0.2.1"]
-    [org.clojure/clojure "1.8.0"]])
+    [ns-tracker "0.3.1"]
+    [org.clojure/clojure "1.10.0"]])
